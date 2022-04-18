79b674c891c7e7a52c0d601365c316a72bad32fc
Evan810828 <1015021084@qq.com>
2022-04-18 18:17:02 +0800

    Update Shop.js

--- frontend/src/app/components/Shop/Shop.js
+++ frontend/src/app/components/Shop/Shop.js
@@ -10,7 +10,7 @@ export function getCookie(name)
 {
     var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
  
-    if(arr=document.cookie.match(reg))
+    if(arr==document.cookie.match(reg))
  
         return unescape(arr[2]);
     else
