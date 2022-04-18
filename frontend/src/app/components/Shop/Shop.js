0f89346bf2f98dba991a2ae1042aa1d02c38080d
Corleonedele <1291683680@qq.com>
2022-04-18 15:57:53 +0800

    修改了shop前端借口

--- frontend/src/app/components/Shop/Shop.js
+++ frontend/src/app/components/Shop/Shop.js
@@ -6,35 +6,88 @@ import { setCookie } from '../Login/cookie'
 
 import './Shop.css'
 
-var Shop = () =>{
+const Shop = () =>{
+    const [ avatar_list, setAvatarList ] = React.useState([])
     
+    //TODO
+    const getAvatarList = ()=>{
+        setCookie("update",new Date().toUTCString())
+        fetch(`/user/avatar/`,{
+            method:'GET',
+        }).then(res=>{
+            if(res.status===200){
+                return res.json()
+            }else{
+                message.warn("get Avatar list Fail")
+            }
+        }).then(data=>{
+            setAvatarList(data.result)
+        })
+    }
 
+    //TODO
+    const getMineList = ()=>{
+        setCookie("update",new Date().toUTCString())
+        fetch(`/user/avatar/`,{
+            method:'GET',
+        }).then(res=>{
+            if(res.status===200){
+                return res.json()
+            }else{
+                message.warn("get Avatar list Fail")
+            }
+        }).then(data=>{
+            setAvatarList(data.result)
+        }) 
+    }
 
+    //TODO
+    const uploadAvatarList = ()=>{
+        setCookie("update",new Date().toUTCString())
+        fetch(`/user/avatar/`,{
+            method:'POST',
+            body:JSON.stringify({
+                title: title,
+                src: src,
+            })
+        }).then(res=>{
+            if(res.status===200){
+                return res.json()
+            }else{
+                message.warn("get Avatar list Fail")
+            }
+        }).then(data=>{
+            setAvatarList(data.result)
+        })
+    }
+    React.useEffect(()=>{
+        getAvatarList()
+    },[])
 
 
     return(
         <div className='Shop'>
-                <button className='back' onClick={()=>{ window.history.back(-1)}}>Back</button>
-
+                <button className='back' onClick={()=>
+                    { window.history.back(-1)}}>Back
+                </button>
 
                 <h1>Avatar's Shop</h1>
             <div className='shop-window'>
 
                 <div className='shop-choice-container'>
-                    <button>Avatar</button>
+                    <button onClick={()=>{getAvatarList();console.log("----");}}>Avatar</button>
 
                     <button>Mine</button>
                 </div>
                 <div className='shop-items-container'>
-                    {[1,1,1,1,1,1].map(verse=>{
+                    {avatar_list.map(verse=>{
                             return(
                                 <div className='ChatRooms-VerseList-Verse' >
                                     <div className='ChatRooms-VerseList-Verse-header'>
-                                        {/* {verse.title} */}
-                                        Avatar
+                                        Avatar's Name {verse.title}
                                     </div>
                                     <div className='ChatRooms-VerseList-Verse-content'>
-                                        hihi
+                                        Click {verse.src} to Download
                                     </div>
                                 </div>
                             )
